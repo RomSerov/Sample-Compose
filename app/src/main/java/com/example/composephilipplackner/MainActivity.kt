488a29f470331a7ceaf19c0eb2a3f8806e768512
grey- <grey-552@yandex.ru>
2023-02-21 22:14:30 +0300

    showSnackbar

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,14 +3,59 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePhilippLacknerTheme {
+            val scaffoldState = rememberScaffoldState()
 
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+
+            val scope = rememberCoroutineScope()
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = textFieldState,
+                        label = {
+                                Text(text = "Введите имя")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Привет, $textFieldState")
+                            }
+                        }) {
+                        Text(text = "Нажми, чтоб поздороваться")
+                    }
+                }
             }
         }
     }
