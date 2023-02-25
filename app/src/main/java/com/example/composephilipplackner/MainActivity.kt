3179496fcb0268267e4ba82ef6693801aae773e8
grey- <grey-552@yandex.ru>
2023-02-25 10:59:59 +0300

    itemsIndexed

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,17 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePhilippLacknerTheme {
-
+                LazyColumn {
+                    itemsIndexed(
+                        items = listOf("Первый", "Второй", "Третий", "Четвертый")
+                    ) { index, item ->
+                        Text(
+                            text = item
+                        )
+                    }
+                }
             }
         }
     }
