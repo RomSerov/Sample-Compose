098a88108e77911a1b340ed8078660dc888731a2
grey- <grey-552@yandex.ru>
2023-02-25 21:44:20 +0300

    simple animation

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,21 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +26,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePhilippLacknerTheme {
 
+                var sizeState by remember {
+                    mutableStateOf(200.dp)
+                }
+                val size by animateDpAsState(
+                    targetValue = sizeState,
+                    tween(
+                        durationMillis = 3000,
+                        delayMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                )
+
+                //для бесконечной анимации
+                val infiniteTransition = rememberInfiniteTransition()
+                val color by infiniteTransition.animateColor(
+                    initialValue = Color.Red,
+                    targetValue = Color.Green,
+                    animationSpec = infiniteRepeatable(
+                        tween(durationMillis = 2000),
+                        repeatMode = RepeatMode.Reverse
+                    )
+                )
+
+                Box(
+                    modifier = Modifier
+                        .size(size)
+                        .background(color),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = { sizeState += 50.dp }) {
+                        Text(text = "Увеличить размер")
+                    }
+                }
             }
         }
     }
