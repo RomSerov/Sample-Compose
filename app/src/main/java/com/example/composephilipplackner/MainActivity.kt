cf9af16b4ae207d95bbebbd822a3ec585391b165
grey- <grey-552@yandex.ru>
2023-03-08 18:50:09 +0300

    navigation easy example

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +19,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePhilippLacknerTheme {
-
+                Navigation()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+
+    var text by remember {
+        mutableStateOf("")
+    }
+    
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(value = text, onValueChange = {
+            text = it
+        }, modifier = Modifier.fillMaxWidth())
+        
+        Spacer(modifier = Modifier.height(8.dp))
+        
+        Button(
+            onClick = { navController.navigate(Screen.DetailScreen.withArgs(text)) },
+            modifier = Modifier.align(alignment = Alignment.End)
+        ) {
+            Text(text = "To Detail Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(name: String?) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Hello, $name")
     }
 }
---
+++ app/src/main/java/com/example/composephilipplackner/Navigation.kt
@@ -0,0 +1,44 @@
+package com.example.composephilipplackner
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = "Rom"
+                    nullable = true
+                }
+            )
+        ) {
+            DetailScreen(name = it.arguments?.getString("name"))
+        }
+    }
+}
+
+sealed class Screen(val route: String) {
+    object MainScreen: Screen(route = "main_screen")
+    object DetailScreen: Screen(route = "detail_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach {
+                append("/$it")
+            }
+        }
+    }
+}
