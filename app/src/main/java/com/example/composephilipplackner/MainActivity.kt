c43c56a7bd9fe28b20010e7638cdccb7621687bc
grey- <grey-552@yandex.ru>
2023-03-09 22:40:20 +0300

    Badged Box

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,29 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +33,118 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePhilippLacknerTheme {
-
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavBar(
+                            items = listOf(
+                                BottomNavItem(name = "Home", route = "home", icon = Icons.Default.Home),
+                                BottomNavItem(name = "Chat", route = "chat", icon = Icons.Default.Notifications, badgeCount = 23),
+                                BottomNavItem(name = "ettings", route = "settings", icon = Icons.Default.Settings)
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                ) {
+                    Navigation(navHostController = navController)
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Navigation(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = "home") {
+
+        composable("home") {
+            HomeScreen()
+        }
+        composable("chat") {
+            ChatScreen()
+        }
+        composable("settings") {
+            SettingsScreen()
+        }
+    }
+}
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector,
+    val badgeCount: Int = 0
+)
+
+@Composable
+fun BottomNavBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+
+    val backStackEntry = navController.currentBackStackEntryAsState()
+
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { bottomNavItem ->
+
+            val selected = bottomNavItem.route == backStackEntry.value?.destination?.route
+
+            BottomNavigationItem(
+                selected = selected,
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.LightGray,
+                onClick = { onItemClick(bottomNavItem) },
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        if (bottomNavItem.badgeCount > 0) {
+                            BadgedBox(badge = {
+                                Text(text = bottomNavItem.badgeCount.toString())
+                            }) {
+                                Icon(imageVector = bottomNavItem.icon, contentDescription = null)
+                            }
+                        } else {
+                            Icon(imageVector = bottomNavItem.icon, contentDescription = null)
+                        }
+                        if (selected) {
+                            Text(
+                                text = bottomNavItem.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Home Screen")
+    }
+}
+
+@Composable
+fun ChatScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Chat Screen")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Settings Screen")
     }
 }
