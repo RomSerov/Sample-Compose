11e47c79cac1ce35c19d41b36b743939f2c3869f
grey- <grey-552@yandex.ru>
2023-03-11 10:50:36 +0300

    Multi-Select LazyColumn

---
+++ app/src/main/java/com/example/composephilipplackner/ListItem.kt
@@ -0,0 +1,6 @@
+package com.example.composephilipplackner
+
+data class ListItem(
+    val title: String,
+    val isSelected: Boolean
+)
--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,21 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +26,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePhilippLacknerTheme {
 
+                var items by remember {
+                    mutableStateOf(
+                        (1..20).map {
+                            ListItem(
+                                title = "Item $it",
+                                isSelected = false
+                            )
+                        }
+                    )
+                }
+
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(items.size) {
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                items = items.mapIndexed { index, listItem ->
+                                    if (it == index) {
+                                        listItem.copy(isSelected = !listItem.isSelected)
+                                    } else listItem
+                                }
+                            }
+                            .padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(text = items[it].title)
+                            if (items[it].isSelected) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = null,
+                                    tint = Color.Green,
+                                    modifier = Modifier.size(20.dp)
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
