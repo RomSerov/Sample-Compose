658b9b56f035e475fde237fa92ee03ea8e400592
grey- <grey-552@yandex.ru>
2023-03-12 11:32:15 +0300

    Custom theme

--- app/src/main/java/com/example/composephilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/composephilipplackner/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.composephilipplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.composephilipplackner.ui.spacing
 import com.example.composephilipplackner.ui.theme.ComposePhilippLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +15,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePhilippLacknerTheme {
-
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(MaterialTheme.spacing.medium)
+                ) {
+                    //content
+                }
             }
         }
     }
